1f1c228ef86bde61e4eabf7980b3ac93c51a73c1
pmpr.dev <pmpr.dev@gmail.com>
2024-09-14 20:03:15 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66ce113e51a4a             |
+    |             66e5ebdf93b24             |
     |_______________________________________|
 */
- use Pmpr\Utility\Document\Document; use Pmpr\Utility\Document\PDF; Document::symcgieuakksimmu(); if (!function_exists("\x70\x72\x5f\165\164\151\x6c\x69\x74\171\x5f\144\157\143\x75\155\x65\x6e\164\x5f\147\145\x74\137\x70\144\146")) { function pr_utility_document_get_pdf() : PDF { return PDF::symcgieuakksimmu(); } }
+ use Pmpr\Utility\Document\Document; use Pmpr\Utility\Document\PDF; Document::symcgieuakksimmu(); if (!function_exists("\160\x72\137\x75\x74\151\154\151\164\x79\137\144\x6f\143\165\x6d\x65\156\164\x5f\x67\145\164\137\160\x64\x66")) { function pr_utility_document_get_pdf() : PDF { return PDF::symcgieuakksimmu(); } }
--- src/Pmpr/Utility/Document/Container.php
+++ src/Pmpr/Utility/Document/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66ce113e51a4a             |
+    |             66e5ebdf93b24             |
     |_______________________________________|
 */
  namespace Pmpr\Utility\Document; use Pmpr\Common\Foundation\Container\LightContainer; class Container extends LightContainer { }
